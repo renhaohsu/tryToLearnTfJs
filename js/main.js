217440b6ff4c15abe72f8137f7ca57a8cd2c0b64
zesterisk <34151904+zesterisk@users.noreply.github.com>
2018-07-01 16:42:37 +0800

    d

--- js/main.js
+++ js/main.js
@@ -46,18 +46,7 @@ drawBackground()
 
 // canvas.addEventListener('click', function(e){console.log(e.offsetX,e.offsetY)},false)
 
-// function map(n, start1, stop1, start2, stop2, withinBounds) {
 function map(n, start1, stop1, start2, stop2) {
-  // p5._validateParameters('map', arguments);
-  // var newval = (n - start1) / (stop1 - start1) * (stop2 - start2) + start2;
-  // if (!withinBounds) {
-  //   return newval;
-  // }
-  // if (start2 < stop2) {
-  //   return this.constrain(newval, start2, stop2);
-  // } else {
-  //   return this.constrain(newval, stop2, start2);
-  // }
   return (n - start1) / (stop1 - start1) * (stop2 - start2) + start2;
 };
 
